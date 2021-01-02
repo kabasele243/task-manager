fda9b69e62331e9601a7d149085c126e103b20d4
Franck Kabasele <kabasele467@gmail.com>
2021-01-02 16:54:57 -0500

    Feat: Setting up Server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('Bouya!!!!!!')
+})
+
+app.get('/users', (req, res) => {
+    console.log(req.body)
+    res.send('Bouya!!!!!!')
+})
+
+app.delete('/users', (req, res) => {
+    console.log(req.body)
+    res.send('Bouya!!!!!!')
+})
 
 app.listen(port, ()=>{
     console.log(`App is running on port ${port}`)
