fc8182ede3331b490fd4f0557b44d7e0163e4c09
Franck Kabasele <kabasele467@gmail.com>
2021-01-02 18:51:51 -0500

    Feat: Add method to Task and User Ressources

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true });
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    done: {
-        type: Boolean,
-        default: false
-    }
-})
+
+
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose')
 
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,21 +20,84 @@ app.post('/users', (req, res) => {
     
     user.save()
         .then((user) => {
-       res.send(user)})
+            res.status(201).send(user)})
         .catch((error) => {
-        res.status(400).send(error)
+            res.status(400).send(error)
         
     })
 })
 
 app.get('/users', (req, res) => {
-    console.log(req.body)
-    res.send('Bouya!!!!!!')
+
+    User.find({}).then((user) => 
+        res.status(200)
+        .send(user))
+        .catch((error) => {
+            res.send(400).send(error)
+        })
+})
+
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        // if(!user){
+        //     return res.status(404)
+        // }
+        res.send(user)
+    }).catch((error) => {
+        res.status(404).send(error)
+    })
+
+    
+    
 })
 
 app.delete('/users', (req, res) => {
     console.log(req.body)
     res.send('Bouya!!!!!!')
+})
+
+
+
+
+
+
+app.post('/tasks', (req, res) => {
+    const task = new Task({
+        description: req.body.description,
+        done: req.body.done
+    })
+
+    task.save()
+        .then((task) => {
+            res.status(201).send(task)})
+        .catch((error) => {
+            res.status(400).send(error)
+    })
+
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((task) => {
+        res.status(200).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.status(200).send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
 })
 
 app.listen(port, ()=>{
