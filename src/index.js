ce9e229aa1882e5feca32ea9a2ae604cce7ca3cb
Franck Kabasele <kabasele467@gmail.com>
2021-01-02 17:11:16 -0500

    Add: User Post End Point

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+require('./db/mongoose')
+
+const User = require('./models/user')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,8 +9,21 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-    console.log(req.body)
-    res.send('Bouya!!!!!!')
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        age: req.body.age
+    })
+    
+    
+    user.save()
+        .then((user) => {
+       res.send(user)})
+        .catch((error) => {
+        res.status(400).send(error)
+        
+    })
 })
 
 app.get('/users', (req, res) => {
