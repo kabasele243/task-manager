b07a109abf290275613466079f891403650209dd
Franck Kabasele <kabasele467@gmail.com>
2021-01-03 12:15:32 -0500

    Hashing Password

--- src/router/user.js
+++ src/router/user.js
@@ -63,7 +63,16 @@ router.patch('/users/:id', async (req,res) => {
     const _id = req.params.id
 
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {new:true, runValidators: true})
+        //Enable Mongoose Middleware to Run
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => {
+                user[update] = req.body[update]
+        })
+
+        await user.save()
+
+        // const user = await User.findByIdAndUpdate(_id, req.body, {new:true, runValidators: true})
         if(!user){
             return res.status(404).send()
         }
