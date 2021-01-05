d925834959fd4558099e949de198f8e321f68788
Franck Kabasele <kabasele467@gmail.com>
2021-01-04 19:42:26 -0500

    enable logout user

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ const taskRouter = require('./router/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-   
-})
+
 
 app.use(express.json())
 app.use(userRouter)
--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
@@ -21,7 +22,7 @@ router.post('/users', async (req, res) => {
     
 })
 
-router.post('/users/login', async (req, res) => {
+router.post('/users/login', auth, async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
@@ -32,7 +33,31 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.post('/users/logout', auth , async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch(error){
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req,res) => {
+    
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(error){
+        res.status(500).send()
+    }
+})
+
+
+router.get('/users', auth, async (req, res) => {
 
     try{
         const users = await User.find({})
@@ -41,6 +66,10 @@ router.get('/users', async (req, res) => {
         res.status(400).send(error)
     }
 
+})
+
+router.get('/users/me', auth, async(req, res) => {
+    res.send(user)
 })
 
 
